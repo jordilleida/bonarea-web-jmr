4fc0e1b21b9a20f693769c11fc07f478ec53da75
jordilleida <jordimimbrera@gmail.com>
2022-03-14 11:26:29 +0100

    server config

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/hello-world-bonarea'));
+app.use(express.static(__dirname + '/dist/bonarea-web-jmr'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/hello-world-bonarea/index.html'));});
+'/dist/bonarea-web-jmr/index.html'));});
 app.listen(process.env.PORT || 8080);
